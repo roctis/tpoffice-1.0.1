b9a1514674da4058b8a8c578e1d9b265e0e92451
roctis <roctis@hotmail.com>
2015-08-05 13:09:45 -0400

    Revert "refined"
    This reverts commit 14c6a93d0635bf797d7a5664500fd4e3fe0942f3.
    
    reverted to add files...

---
+++ tpbox.py
@@ -0,0 +1,285 @@
+#!/usr/bin/python          
+
+#Class Declaration
+class TCP_connection_CLIENT:
+    'class for connection oriented connected for file transfer'
+    tcp_conn_count=0
+    
+    def __init__(self, (host_port), buffer_size):
+        self.host_port=host_port
+        self.buffer_size=buffer_size
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        TCP_connection_CLIENT.tcp_conn_count+=1
+        
+    def connection_establish(self):
+        self.s.connect(self.host_port)
+        
+    def receive_file(self, filename):
+        #self.s.settimeout(2.0)
+        f=file(filename,'wb')
+        i=0
+        try:
+            data = self.s.recv(self.buffer_size)
+            print "Receiveing.",
+            delim=str(data) #delimiter for the TCP connection...
+            while data:
+                i+=1
+                if delim.find("@!&^")!=-1:
+                    f.write(data[:-4])
+                    break
+                else:
+                    f.write(data)
+                    data = self.s.recv(self.buffer_size)
+                    delim=str(data)
+                if (i%100==0):
+                    print ".",
+           
+            f.close()
+            print "[100%]"
+        except socket.timeout:
+            print "client 27"
+        print "Received File: %s" % (filename)
+    
+        
+    def send_file(self, filename):
+        i=0
+        try:
+            f=open(filename, 'rb')
+        except: 
+            print "UNABLE TO READ THE FILE"
+            pass
+        data=f.read(1024)
+        print "\nSending "+filename+".",
+    #try:
+        while data:
+            i+=1
+            if (i%100==0):
+                    print ".",
+            self.s.send(data)
+            data=f.read(self.buffer_size)
+                
+        f.close()
+        print "[100%]\n[SENDING COMPLETED]\n"
+#        except socket.error:
+#            print "client 45"
+#            pass
+#    
+        
+    def close_socket(self):
+        self.s.close()
+    
+
+class UDP_connection_CLIENT:
+        'Class for connection less connection for message transfer'
+        udp_conn_count=0
+        def __init__(self, (host_port), buffer_size):
+            self.server=host_port
+            self.buffer_size=buffer_size
+            self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            UDP_connection_CLIENT.udp_conn_count+=1
+            
+        def receive_message(self):
+            'receive udp message'
+            self.data,self.addr = self.s.recvfrom(self.buffer_size)
+            self.data=str(self.data)
+            return (self.data, self.addr)
+            
+        def send_message(self, data):
+            'receive udp message'
+            self.s.sendto(data, self.server)
+            
+        def display_message(self):
+            print self.data
+            
+        def close_socket(self):
+            self.s.close()
+    
+            
+
+#FLAGS
+save_flag = False
+shutdown=False
+file_tansfer=False
+
+#FUNCTIONS
+
+#CHECK_OPTIONS
+
+def check_option_for_saving(option):
+    #saving to file
+    global save_flag
+    global alias
+    if option.find('>save<')!=-1:
+        if not save_flag:
+            print "SAVING \'data/"+alias+"txt\'" 
+        else:
+            print "COMPLETED...............................................[100%]\n"
+        save_flag=not save_flag
+        
+    if save_flag:
+        save_to_file(alias, option)
+    
+    #saving completed
+    
+
+#SAVING FUNCTION
+def save_to_file(filename, data):
+    path='data/'+filename+'.txt'
+    if not os.path.isdir('data'): os.makedirs('data')
+    f=open(path, 'a')
+    f.write(data)
+    f.write('\n')
+    f.close()
+
+def get_filename(path):
+    path.split
+    filename=path.rsplit('/', 1)
+    filename=filename[-1]
+    return filename
+        
+def get_filename_from_text(data):
+    fname=data.split('#')
+    return fname[1]
+    
+def get_client_temp_path(fname, dir):
+    if not os.path.isdir(dir): os.makedirs(dir)
+    fname=dir+fname
+    return fname
+
+#RECEIVING FUNCTION
+
+def receving_message(name, text,  doc):
+    global filename
+    global shutdown
+    global save_flag
+    global alias
+    while not shutdown:
+        try:
+            tLock.acquire()
+            while True:
+                if shutdown:
+                    sys.exit()
+                msg=text.receive_message()
+                if msg[0].find('@!file@!')!=-1:
+                    file_name=get_filename_from_text(msg[0])
+                    file_name=get_client_temp_path(file_name, 'downloaded/')
+                    doc.receive_file(file_name)
+                else:
+                    text.display_message()
+                if save_flag:save_to_file(alias, msg[0])
+                
+        except:
+            pass
+        finally:
+            tLock.release()
+            
+        time.sleep(0.2)
+
+
+        
+    
+#MAIN FUNCTION STARTS HERE#
+if __name__ == '__main__':
+    import sys
+    import getopt
+    import os
+    import socket
+    import time
+    import threading
+    print
+    print 'USAGE:tfbox.py <server_ipaddress>'
+    print 'press >save< to save the conversation'
+    print "press >file< to send the file to all clients"
+    print "press >fileto< to send the file to specific ip"
+    print "press >filetos< to send the file to specific ip"
+    print "press >xtheserver< to terminate the server"
+    print "press [CTRL+Z] to exit the terminal"
+    print "press [CTRL+SHIFT+C] to copy the selected part in terminal"
+    print "press [CTRL+SHIFT+V] to paste in the terminal"
+    from optparse import OptionParser
+    usage = "usage: %prog [options] server_ipaddress"
+    parser = OptionParser(usage=usage)
+    #parser.add_option("-s", "--server", action="store", type="string", dest="save", default="100",
+    #    help="yet to be implemented")
+    #if options.save:
+    #   [code here]
+    
+    args, sources = getopt.getopt(sys.argv[1:], 'f:', 'shotdir=')
+    #args = dict(args)
+    if len(sources)==0:host='127.0.0.1'
+    else:host=sources[0]
+
+    #global variables
+    HOST =host
+    PORT = 5000
+    host_port=(HOST, PORT)
+    buffer_size=8000
+    
+    
+    
+    #program
+#    path=raw_input("\nEnter the path of file:\n ")
+#    filename=get_filename(path)
+#    TCP_connection_CLIENT(host_port, buffer_size).tcp_send_file(path)
+#   
+    tLock = threading.Lock()
+    text=UDP_connection_CLIENT(host_port, buffer_size)
+    doc= TCP_connection_CLIENT(host_port, buffer_size)
+    doc.connection_establish()
+    rT = threading.Thread(target=receving_message, args=("receive_msg",text, doc))
+    rT.start()
+    alias=str(raw_input("\nName: "))
+    text.send_message(alias+" has joined in")
+ 
+    while not shutdown:
+        msg=str(raw_input("[self] "))
+        if msg=='>bye<':
+            shutdown=True
+        #saving to file
+        elif msg=='>save<': save_flag=not save_flag
+        elif save_flag:
+            save_to_file(alias, msg)
+        #saving completed
+        #sendig file
+        elif msg.find('>file<')!=-1:
+            #file_transfer=True
+            print
+            path=str(raw_input("Enter the path of file e.g: 'data/foo.txt'  :\n"))
+            path=path.strip(" ")
+            path=path.strip("\'")
+            filename=get_filename(path)
+            text.send_message('@!file@!'+'#'+filename)
+            doc.send_file(path)
+        elif msg.find('>filetos<')!=-1:
+            #file_transfer=True
+            print
+            path=str(raw_input("Enter the path of file e.g: 'data/foo.txt'  :\n"))
+            path=path.strip(" ")
+            path=path.strip("\'")
+	    filename=get_filename(path)
+            text.send_message('@!filetos@!'+'#'+filename)
+            doc.send_file(path)
+        elif msg.find('>fileto<')!=-1:
+            file_transfer=True
+            print
+            path=str(raw_input("Enter the path of file e.g: 'data/foo.txt'  :\n"))
+            path=path.strip(" ")
+            path=path.strip("\'")
+	    filename=get_filename(path)
+            print
+            address=str(raw_input("Enter the ip address of destination e.g: 192.168.0.34  :\n"))
+            path_ip=str('@!fileto@!'+'#'+filename+"#"+address.strip())
+            print
+            text.send_message(path_ip)
+
+        #sendig file ends here
+        else:
+            msg='['+alias+'] '+msg
+            text.send_message(msg)
+        time.sleep(0.2)
+    rT.join()    
+    text.close_socket()
+    doc.close_socket()
+    exit()
+    
+
---
+++ tpserver.py
@@ -0,0 +1,280 @@
+#!/usr/bin/python     
+#Declaring object
+
+class TCP_connection_SERVER:
+    'class for connection oriented connected for file transfer'
+    tcp_conn_count=0
+    clients_addrs=[]
+    def __init__(self, (host_port), buffer_size, backlog):
+        self.host_port=host_port
+        self.buffer_size=buffer_size
+        self.backlog=backlog
+        #create tcp socket and binding
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.s.bind(host_port)
+        self.s.listen(backlog)
+        
+    
+    def connection_establish(self):
+        self.s.setblocking(0)
+        try:
+            client, addr =self.s.accept()
+            clients_addrs=(client, addr)
+            #print addr
+            if clients_addrs not in TCP_connection_SERVER.clients_addrs:
+               TCP_connection_SERVER.clients_addrs.append(clients_addrs)
+               
+            TCP_connection_SERVER.tcp_conn_count+=1
+        except:
+            pass
+            
+        return True
+     
+     
+    def receive_send_file(self, filename, address=False,dest_address=False,only_to_server=False):
+        present_client, present_addr=self.get_req_socket(address)
+        present_client.settimeout(2.0)
+        f=file(filename,'wb')
+        try:
+            data = present_client.recv(self.buffer_size)
+            while data:
+                f.write(data)
+                data = present_client.recv(self.buffer_size)
+        except socket.timeout:
+            f.close()
+            print "Received File: %s" % (filename)
+	if len(TCP_connection_SERVER.clients_addrs)>1 and not only_to_server:
+            self.send_file(filename, present_client,dest_address)
+	else:
+	    print "Transfered to server.....[100%]"
+        
+
+        
+                
+    def send_file(self,filename, present_client, addr=False):
+        print "Sending file to ",
+        if not addr:
+	    print "all "
+            for available_client , available_addr in TCP_connection_SERVER.clients_addrs:
+                if(present_client!=available_client):
+                    f=open(filename, 'rb')
+                    data=f.read(1024)
+                    try:
+                        while data:
+                            available_client.send(data)
+                            data=f.read(1024)
+                        f.close()
+                        available_client.send("@!&^")
+                    except socket.error:
+                        print "Removing Client"
+                        self.rm_dead_socket(available_addr)
+                        continue
+        
+        else:
+		
+            dest_client, dest_addr=self.get_req_socket(addr)
+	    print dest_addr
+            f=open(filename, 'rb')
+            data=f.read(1024)
+            try:
+            	while data:
+                	dest_client.send(data)
+                        data=f.read(1024)
+                f.close()
+                dest_client.send("@!&^")
+            except socket.error:
+                print "No Requested Client Available"
+                self.rm_dead_socket(available_addr)
+
+        if rm_server_temp_files:
+            os.remove(filename)
+
+            
+        
+            
+                
+    def get_req_socket(self, addr):
+        
+        i=-1
+        flag=False
+        if len(TCP_connection_SERVER.clients_addrs)>1:
+            for sending_client, sending_addr in TCP_connection_SERVER.clients_addrs:
+                i+=1
+                #print sending_addr[0]
+                #print addr[0]
+                if sending_addr[0]==addr[0]:
+                    flag=True
+                    break
+            return TCP_connection_SERVER.clients_addrs[i]
+            
+        else:
+            return TCP_connection_SERVER.clients_addrs[0]
+
+    def rm_dead_socket(self, addr):
+
+        i=-1
+        flag=False
+        if len(TCP_connection_SERVER.clients_addrs)>1:
+            for listed_client, listed_addr in TCP_connection_SERVER.clients_addrs:
+                i+=1
+                #print sending_addr[0]
+                #print addr[0]
+                if listed_addr==addr:
+                    flag=True
+                    break
+            del TCP_connection_SERVER.clients_addrs[i]
+
+        else:
+            del TCP_connection_SERVER.clients_addrs[0]
+            
+        
+                
+    def add_clients(self, client):
+        if client not in TCP_connection_SERVER.clients:
+            TCP_connection_SERVER.clients.append(client)    
+        #return TCP_connection_SERVER.clients
+        
+    def close_socket(self):
+        self.s.close()
+    
+    
+
+
+class UDP_connection_SERVER:
+    'Class for connection less connection for message transfer'
+    udp_conn_count=0
+    clients=[]
+    def __init__(self, (host_port), buffer_size):
+        self.host_port=host_port
+        self.buffer_size=buffer_size
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.s.bind(host_port)
+        UDP_connection_SERVER.udp_conn_count+=1
+        
+    def receive_message(self):
+        'receive udp message'
+        self.data,self.addr = self.s.recvfrom(self.buffer_size)
+        self.data=str(self.data)
+	self.add_client_address(self.addr);
+        return (self.data, self.addr)
+               
+    def add_client_address(self,addr):
+        if addr not in UDP_connection_SERVER.clients:
+            UDP_connection_SERVER.clients.append(addr)
+        return UDP_connection_SERVER.clients
+        
+    def display_message(self):
+        print "["+time.ctime(time.time())+str(self.addr)+"]"+self.data
+        
+    def send_to_clients(self, msg=False):
+        if msg:
+            self.data=msg
+        for client in UDP_connection_SERVER.clients:
+            if client !=self.addr:
+                try:
+                    self.s.sendto(self.data,  client)
+                     
+                except:
+	 	    #self.rm_dead_client(self,addr)
+		    continue     
+  
+    
+    
+    def close_socket(self):
+        self.s.close()
+            
+
+
+#Functions
+
+#def tcp_send_file(self,data):
+        
+        
+def get_server_temp_path(fname, dir):
+    if not os.path.isdir(dir): os.makedirs(dir)
+    fname=dir+fname
+    return fname
+
+def get_filename(data):
+    fname=data.split('#')
+    return fname[1]
+
+def get_address(data):
+    fname=data.split('#')
+    return fname[2]
+
+def get_ip_address(ifname):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    return socket.inet_ntoa(fcntl.ioctl(
+        s.fileno(),
+        0x8915,  # SIOCGIFADDR
+        struct.pack('256s', ifname[:15])
+    )[20:24])
+    
+
+
+if __name__ == '__main__':
+    import sys
+    import getopt
+    import os
+    import time
+    import socket
+    import fcntl
+    import struct
+    import select
+
+    
+    print
+    #args, sources = getopt.getopt(sys.argv[1:])
+    
+
+#FLAGS
+    rm_server_temp_files=True
+    server_shutdown=False
+    
+#global variables
+    HOST =get_ip_address('wlan0')
+    PORT = 5000
+    host_port=(HOST, PORT)
+    buffer_size=8000
+    backlog=5
+    
+ #program
+    print "SERVER started with ip="+HOST
+    text=UDP_connection_SERVER(host_port, buffer_size)
+    doc=TCP_connection_SERVER(host_port, buffer_size, backlog)
+    
+ 
+    while not server_shutdown:
+        doc.connection_establish()
+        msg=text.receive_message()
+        if msg[0].find('@!file@!')!=-1:
+            file_name=get_filename(msg[0])
+            text.send_to_clients("@!file@!#"+file_name)
+            file_name=get_server_temp_path(file_name, 'server-dl/')
+            doc.receive_send_file(file_name, msg[1],False, only_to_server=False)
+        elif msg[0].find('@!fileto@!')!=-1:
+            file_name=get_filename(msg[0])
+	    dest_address=get_address(msg[0])
+            text.send_to_clients("@!file@!#"+file_name)
+            file_name=get_server_temp_path(file_name, 'server-dl/')
+            doc.receive_send_file(file_name, msg[1], dest_address, only_to_server=False)
+        elif msg[0].find('@!filetos@!')!=-1:
+            file_name=get_filename(msg[0])
+            file_name=get_server_temp_path(file_name, 'server-dl/')
+            doc.receive_send_file(file_name, msg[1],False,only_to_server=True )
+        
+        elif msg[0].find('>xtheserver<')!=-1:
+            server_shutdown=True
+            
+        else:
+            
+            text.display_message()
+            send_all=text.send_to_clients()
+        time.sleep(0.2)
+        
+    text.close_socket()
+    doc.close_socket()
+
